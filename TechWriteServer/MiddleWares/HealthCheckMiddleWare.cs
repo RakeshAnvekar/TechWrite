2d5a8d1472824cd592be45124d83c2e0ae07d7c3
rakesh <rakesh_anvekar@gmail.com>
2025-01-29 11:11:30 +0530

    added middlewares

---
+++ TechWriteServer/MiddleWares/HealthCheckMiddleWare.cs
@@ -0,0 +1,30 @@
+﻿namespace TechWriteServer.MiddleWares;
+
+public class HealthCheckMiddleWare
+{
+    #region Private Properties
+    private readonly RequestDelegate _next;
+    #endregion
+    #region Constructor
+    public HealthCheckMiddleWare(RequestDelegate next)
+    {
+        _next = next;
+    }
+    #endregion
+    #region MiddleWare method
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.Request.Path == "/healthcheck")
+        {
+            context.Response.Clear();
+            context.Response.StatusCode = StatusCodes.Status200OK;
+            await context.Response.WriteAsync("TechWrite app is healthy");
+            await context.Response.CompleteAsync();
+        }
+        else
+        {
+            await _next(context);
+        }
+    }
+    #endregion
+}
---
+++ TechWriteServer/Models/Others/CommonMiddleWare.cs
@@ -0,0 +1,7 @@
+﻿namespace TechWriteServer.Models.Others;
+
+public class CommonMiddleWare
+{
+public int Code {  get; set; }
+public string CustomMessge {  get; set; }=string.Empty;
+}
